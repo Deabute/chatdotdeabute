fb321a1f12a3378a6283147f8f15bdce9568ce42
PaulBeaudet <inof8or@gmail.com>
2019-04-06 16:39:08 -0400

    change type to action

--- src/deabute.js
+++ src/deabute.js
@@ -12,23 +12,24 @@ var wsDeabute = {
             wsDeabute.active = true;
             wsDeabute.connected = true;
             wsDeabute.instance.onmessage = wsDeabute.incoming;
-            wsDeabute.send({type: 'connected', oid: localStorage.oid, lastMatches: rtc.lastMatches});
+            wsDeabute.send({action: 'connected', oid: localStorage.oid, lastMatches: rtc.lastMatches});
             wsDeabute.onclose = function onSocketClose(){wsDeabute.connected = false;};
             onConnection = onConnection ? onConnection : wsDeabute.onConnection;
             onConnection();
         };
     },
     incoming: function(event){           // handle incoming socket messages
-        var req = {type: null};          // request
+        console.log(event.data);
+        var req = {action: null};          // request
         try {req = JSON.parse(event.data);}catch(error){}
-        if(req.type === 'msg'){
+        if(req.action === 'msg'){
             console.log('incomming socket message: ' + req.msg);
-        } else if(req.type === 'fail'){
+        } else if(req.action === 'fail'){
             // don't do the thing
         }
     },
     send: function(msg){
-        try{msg = JSON.stringify(msg);} catch(error){msg = {type:'error', error: error};}
+        try{msg = JSON.stringify(msg);} catch(error){msg = {action:'error', error: error};}
         if(wsDeabute.connected){
             wsDeabute.instance.send(msg);
             return true;
@@ -62,7 +63,7 @@ var deabute = {
         var password = deabute.lpassword.value;
         if(username && password){
             deabute.loginBox.hidden = true;
-            wsDeabute.send({type: 'login', username: username, password: password});
+            wsDeabute.send({action: 'login', username: username, password: password});
         } else {console.log('missing shit');}
     },
     signup: function(){
@@ -70,7 +71,7 @@ var deabute = {
         var password = deabute.spassword.value;
         if(username && password){
             deabute.signupBox.hidden = true;
-            wsDeabute.send({type: 'signup', username: username, password: password});
+            wsDeabute.send({action: 'signup', username: username, password: password});
         } else {console.log('missing shit');}
     }
 };
